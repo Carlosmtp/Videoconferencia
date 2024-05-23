21a8aec9c9fdf89ccd4290fd126daa64bb0e9443
DiegoFLM <diego.ledesma@correounivalle.edu.co>
2024-05-22 21:10:53 -0500

    Closes #6 - View of the video conference, styles improved.

--- src/pages/video-call/VideoCall.tsx
+++ src/pages/video-call/VideoCall.tsx
@@ -1,40 +1,68 @@
-import React from "react";
+import React, {useRef} from "react";
 import { useNavigate } from "react-router-dom";
 import "./stylesVideoCall.css";
-import { MdCall } from "react-icons/md";
-import { FaMicrophone } from "react-icons/fa";
-import { BsFillCameraVideoFill } from "react-icons/bs";
+// import { MdCall } from "react-icons/md";
+import { MdCallEnd } from "react-icons/md";
+// import { FaMicrophone } from "react-icons/fa";
+import { FaMicrophoneSlash } from "react-icons/fa";
+// import { BsFillCameraVideoFill } from "react-icons/bs";
+import { BsFillCameraVideoOffFill } from "react-icons/bs";
 import { IoSend } from "react-icons/io5";
 
+
+
+
 export default function VideoCall(){
+    const chatHistoryRef = useRef<HTMLTextAreaElement>(null);
+    const messageRef = useRef<HTMLTextAreaElement>(null);
+
+    function onHandleSend(){
+        if (messageRef.current!.value === "" || 
+                messageRef.current!.value === "\n"){
+            messageRef.current!.value = "";
+            return;
+        }
+        chatHistoryRef.current!.value += messageRef.current!.value + "\n";
+        messageRef.current!.value = "";
+    }
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            onHandleSend();
+        }
+    };
 
     return(
         <div className="container">
-            <div className="flex center-item">
-                <div className="video-call-placeholder">
-                    <p>Not active video call</p>
+            <div className="flex center-item margin-bottom-0">
+                
+                <div className="video-and-buttons">
+                    <div className="video-call-placeholder">
+                        <p>No active video call</p>
+                    </div>
+                    <div className="video-call-buttons">
+                        <button className="round-button color-black hover-red">
+                            <MdCallEnd />{/* Start/Finish Video Call */} 
+                        </button>
+                        <button className="round-button color-black hover-red">
+                            <BsFillCameraVideoOffFill />{/* Activate/Deactivate camera */}
+                        </button>
+                        <button className="round-button color-black hover-red">
+                            <FaMicrophoneSlash />{/* Activate/Deactivate Mic */}
+                        </button>
+                    </div>
                 </div>
                 <div className="video-call-chat">
-                    <textarea className="video-call-chat-history"></textarea>
+                    <textarea ref={chatHistoryRef} className="video-call-chat-history" readOnly></textarea>
                     <div className="video-call-chat-input-area">
-                        <input type="text" className="video-call-chat-input-text" placeholder="Type a message" />
-                        <button className="send-button background-color-green color-white">
+                        <textarea ref={messageRef} onKeyDown={handleKeyPress} className="video-call-chat-input-textarea" placeholder="Type a message" />
+                        <button onClick={onHandleSend} className="send-button background-color-green color-white">
                             <IoSend />
                         </button>
                     </div>
                 </div>
-            </div>
-            <div className="flex center-item">
-                <button className="login-button background-color-green color-white hover-darkgreen">
-                    <MdCall />{/* Start/Finish Video Call */} 
-                </button>
-                <button className="login-button background-color-blue color-white hover-darkgreen">
-                    <BsFillCameraVideoFill />{/* Activate/Deactivate camera */}
-                </button>
-                <button className="login-button background-color-green color-white hover-darkblue">
-                    <FaMicrophone />{/* Activate/Deactivate Mic */}
-                </button>
-            </div>
+            </div>            
         </div>
     )
 }
