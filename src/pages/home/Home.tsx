b8d938de86b56790644c4682ee16d0ca2dd5fd35
Carlos Tovar <carlos.mauricio.tovar@correounivalle.edu.co>
2024-06-11 18:32:41 -0500

    styles

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -15,8 +15,8 @@ export default function Home() {
     return (
         <div className="container">
             <div className="header">
-                <h1 className="text-align-center color-red">VideoChat EISC</h1>
-                <span className="text-align-center flex align-items-center">Welcome</span>
+                <h1 className="main-title">VideoChat EISC</h1>
+                <span className="sub-title">Welcome</span>
             </div>
             <div className="container center-item vertical">
                 <button onClick={onHandleLogin} className="login-button">
